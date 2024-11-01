cf228cd2508e27ed30fdb23380e93e2a2e0f7ebd
lesia_onyshchuk <lkane443@gmail.com>
2024-11-01 14:19:51 +0200

    5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorText = document.querySelector('.color');
+const buttonColor = document.querySelector('.change-color');
+const bodyColor = document.querySelector('body');
+
+const colorChange = () => {
+  const color = getRandomHexColor();
+  bodyColor.style.backgroundColor = color;
+  colorText.textContent = color;
+};
+
+buttonColor.addEventListener('click', colorChange);
